cb3aa5fffca4efaab01e66f82f0c8b8a09764883
gautiermarechal <gaut.marechal@gmail.com>
2021-02-16 09:02:04 -0500

    Single recipe almost finished, just miss basic info

--- components/RecipesScreen.js
+++ components/RecipesScreen.js
@@ -22,6 +22,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     width: null,
     backgroundColor: COLORS.blueWhite,
+    height: "auto",
   },
   titleContainer: {
     display: "flex",
--- components/SingleRecipe.js
+++ components/SingleRecipe.js
@@ -6,6 +6,7 @@ import {
   FlatList,
   Button,
   Pressable,
+  ScrollView,
 } from "react-native";
 import { useSelector } from "react-redux";
 import { COLORS } from "../libs/constants";
@@ -21,69 +22,113 @@ const SingleRecipe = ({ navigation }) => {
   }, [singleRecipe]);
   return (
     <>
-      <View style={styles.container}>
-        <View style={styles.ingredientsContainer}>
+      <ScrollView contentContainerStyle={styles.container}>
+        <View style={styles.basicInfoContainer}>
+          <View style={styles.duration}></View>
+        </View>
+        <View style={styles.sectionContainer}>
           <Text style={styles.title}>Ingredients</Text>
-          <View
-            style={{
-              borderBottomColor: "black",
-              borderBottomWidth: 1,
-            }}
-          />
           <View styles={styles.ingredientsList}>
-            <FlatList
-              data={singleRecipe.ingredients}
-              renderItem={({ item }) => (
-                <View style={styles.ingredientItem}>
-                  <View style={styles.listStyle} />
-                  <Text style={styles.ingredientName}>{item.name}</Text>
+            {singleRecipe.ingredients.map((ingredient) => (
+              <View style={styles.listItem} key={ingredient.id}>
+                <View style={styles.listStyle} />
+                <View style={styles.ingredientTagContainer}>
+                  <Text style={styles.ingredientName}>{ingredient.name}</Text>
+                  <Text style={styles.ingredientQuantity}>
+                    {ingredient.quantity.number}
+                    &nbsp;
+                    {ingredient.quantity.unit}
+                  </Text>
                 </View>
-              )}
-            />
+              </View>
+            ))}
           </View>
         </View>
-      </View>
+        <View style={styles.sectionContainer}>
+          <Text style={styles.title}>Preparation</Text>
+          <View styles={styles.ingredientsList}>
+            {singleRecipe.preparation.map((step) => (
+              <View style={styles.listPreparationItem} key={step.step_number}>
+                <Text style={styles.stepNumber}>{step.step_number}</Text>
+                <View style={styles.preparationStepContainer}>
+                  <Text style={styles.preparationStep}>{step.description}</Text>
+                </View>
+              </View>
+            ))}
+          </View>
+        </View>
+      </ScrollView>
     </>
   );
 };
 
 const styles = StyleSheet.create({
+  basicInfoContainer: {
+    flex: 1,
+    flexDirection: "row",
+  },
+  duration: {},
   container: {
     backgroundColor: COLORS.white,
     display: "flex",
     alignItems: "center",
-    height: "100%",
+    height: "auto",
   },
   title: {
     fontSize: 40,
     fontWeight: "600",
+    marginBottom: 15,
   },
-  ingredientsContainer: {
-    display: "flex",
+  sectionContainer: {
+    flex: 1,
     alignItems: "flex-start",
     width: "90%",
     padding: 20,
-    marginLeft: 20,
+    marginTop: 20,
+    marginLeft: 10,
     marginHorizontal: 20,
   },
   ingredientsList: {
     marginTop: 10,
   },
-  ingredientItem: {
+  listItem: {
     flex: 1,
     flexDirection: "row",
-    marginTop: 5,
-    marginBottom: 5,
     alignItems: "center",
+    marginTop: 5,
+    marginBottom: 10,
+  },
+  listPreparationItem: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "flex-start",
+    marginBottom: 5,
   },
   listStyle: {
     width: 15,
-    height: 15,
+    height: 5,
     backgroundColor: COLORS.lightCoral,
-    borderRadius: 50,
   },
   ingredientName: {
+    fontSize: 20,
+    fontWeight: "500",
+  },
+  ingredientQuantity: {
+    fontStyle: "italic",
+    color: COLORS.grey,
+  },
+  ingredientTagContainer: {
     marginLeft: 10,
+  },
+  preparationStepContainer: {
+    marginRight: 40,
+  },
+  stepNumber: {
+    color: COLORS.lightCoral,
+    fontWeight: "800",
+    fontSize: 30,
+    marginRight: 10,
+    marginTop: -5,
   },
 });
 
